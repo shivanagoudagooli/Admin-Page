2a8853046079b889958884dc9c960518d76e7a24
shivanagoudagooli <43290109+shivanagoudagooli@users.noreply.github.com>
2021-11-25 16:10:04 +0530

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app" style="background: #e7e7e7;">
+    <AdminPage msg="ADMIN UI"/>
+  </div>
+</template>
+
+<script>
+import AdminPage from './components/AdminPage.vue'
+
+export default {
+  name: 'App',
+  components: {
+    AdminPage
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# Admin-Page
-Created UI for Admin
+# adminui using Vue JS(vue@2.6.11)
+
+## Project setup(fallowing need to install to ru the code - 
+```
+npm install moment@2.29.1
+npm install node-sass@4.14.1,
+npm install sass-loader@7.3.1,
+npm install v-tooltip@2.1.3,
+npm install vuex@3.6.2
+npm install
+
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
